ca383f5092b8417c95eab1b6005a8a8c1c6f16f5
Donghao Huang <donghao.huang@gmail.com>
2020-08-20 13:45:07 +0800

    updated menu
    Former-commit-id: 0e6e1fe80d59fb64937022bd91a506c1fbe3f996

--- docs/member/js/home.js
+++ docs/member/js/home.js
@@ -1,90 +1,73 @@
 var Parse = window.Parse;
 
 customElements.define(
-  'member-func',
-  class extends HTMLElement {
-    constructor() {
-      super();
-      const template = document.getElementById('member-template');
-      const templateContent = template.content;
-      const shadowRoot = this.attachShadow({ mode: 'open' });
-      shadowRoot.appendChild(templateContent.cloneNode(true));
+	'member-func',
+	class extends HTMLElement {
+		constructor() {
+			super();
+			const template = document.getElementById('member-template');
+			const templateContent = template.content;
+			const shadowRoot = this.attachShadow({ mode: 'open' });
+			shadowRoot.appendChild(templateContent.cloneNode(true));
 
-      this.shadowRoot
-        .querySelector('#logoutButton')
-        .addEventListener('click', this.logout);
-    }
+			this.shadowRoot.querySelector('#logoutButton').addEventListener('click', this.logout);
+		}
 
-    logout() {
-      Parse.User.logOut()
-        .catch(() => {})
-        .then(() => {
-        //   window.location.href = 'index.html';
-        });
-    }
+		logout() {
+			Parse.User.logOut().catch(() => {}).then(() => {
+				window.location.href = '/index.html';
+			});
+		}
 
-    connectedCallback() {
-      // prevent flicker
-      window.addEventListener(
-        'load',
-        () => {
-            if (Parse.User.current()) {
-                this.shadowRoot
-                .querySelector('#member')
-                .setAttribute('style', 'display: block;');
-              } else {
-                this.shadowRoot
-                .querySelector('#non-member')
-                .setAttribute('style', 'display: block;');
-              }
-        },
-        false
-      );
-    }    
-  }
+		connectedCallback() {
+			// prevent flicker
+			window.addEventListener(
+				'load',
+				() => {
+					if (Parse.User.current()) {
+						this.shadowRoot.querySelector('#member').setAttribute('style', 'display: block;');
+					} else {
+						this.shadowRoot.querySelector('#non-member').setAttribute('style', 'display: block;');
+					}
+				},
+				false
+			);
+		}
+	}
 );
 
 customElements.define(
-    'member-menu',
-    class extends HTMLElement {
-      constructor() {
-        super();
-        const template = document.getElementById('member-menu');
-        const templateContent = template.content;
-        const shadowRoot = this.attachShadow({ mode: 'open' });
-        shadowRoot.appendChild(templateContent.cloneNode(true));
-  
-        this.shadowRoot
-          .querySelector('#logoutButton')
-          .addEventListener('click', this.logout);
-      }
-  
-      logout() {
-        Parse.User.logOut()
-          .catch(() => {})
-          .then(() => {
-          //   window.location.href = 'index.html';
-          });
-      }
-  
-      connectedCallback() {
-        // prevent flicker
-        window.addEventListener(
-          'load',
-          () => {
-              if (Parse.User.current()) {
-                  this.shadowRoot
-                  .querySelector('#member')
-                  .setAttribute('style', 'display: block;');
-                } else {
-                  this.shadowRoot
-                  .querySelector('#non-member')
-                  .setAttribute('style', 'display: block;');
-                }
-          },
-          false
-        );
-      }    
-    }
-  );
-  
+	'member-menu',
+	class extends HTMLElement {
+		constructor() {
+			super();
+			const template = document.getElementById('member-menu');
+			const templateContent = template.content;
+			const shadowRoot = this.attachShadow({ mode: 'open' });
+			shadowRoot.appendChild(templateContent.cloneNode(true));
+
+			this.shadowRoot.querySelector('#logoutButton').addEventListener('click', this.logout);
+		}
+
+		logout() {
+			Parse.User.logOut().catch(() => {}).then(() => {
+				window.location.href = '/index.html';
+			});
+		}
+
+		connectedCallback() {
+			// prevent flicker
+			window.addEventListener(
+				'load',
+				() => {
+					if (Parse.User.current()) {
+						this.shadowRoot.querySelector('#member').setAttribute('style', 'display: block;');
+					} else {
+						this.shadowRoot.querySelector('#non-member').setAttribute('style', 'display: block;');
+					}
+				},
+				false
+			);
+		}
+	}
+);
