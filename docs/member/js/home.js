14f70de59e110f19878ef6d0c25f52f152be172d
Donghao Huang <donghao.huang@gmail.com>
2020-08-21 00:58:41 +0800

    fixed display issues
    Former-commit-id: 320911442422024669d287a0b772b114268e2ad8

--- docs/member/js/home.js
+++ docs/member/js/home.js
@@ -1,74 +1,40 @@
 var Parse = window.Parse;
 
-customElements.define(
-	'member-func',
-	class extends HTMLElement {
-		constructor() {
-			super();
-			const template = document.getElementById('member-template');
-			const templateContent = template.content;
-			const shadowRoot = this.attachShadow({ mode: 'open' });
-			shadowRoot.appendChild(templateContent.cloneNode(true));
+document.addEventListener('DOMContentLoaded', () => {
+	const loggedInUser = Parse.User.current();
 
-			this.shadowRoot.querySelector('#logoutButton').addEventListener('click', this.logout);
-		}
+	var element = document.getElementById(loggedInUser ? 'member' : 'non-member');
 
-		logout() {
-			Parse.User.logOut().catch(() => {}).then(() => {
-				window.location.href = '/index.html';
-			});
-		}
-
-		connectedCallback() {
-			// prevent flicker
-			window.addEventListener(
-				'load',
-				() => {
-					if (Parse.User.current()) {
-						this.shadowRoot.querySelector('#member').setAttribute('style', 'display: block;');
-					} else {
-						this.shadowRoot.querySelector('#non-member').setAttribute('style', 'display: block;');
-					}
-				},
-				false
-			);
-		}
+	if (element) {
+		element.setAttribute('style', 'display: block;');
 	}
-);
 
-customElements.define(
-	'member-menu',
-	class extends HTMLElement {
-		constructor() {
-			super();
-			const template = document.getElementById('member-menu');
-			const templateContent = template.content;
-			const shadowRoot = this.attachShadow({ mode: 'open' });
-			shadowRoot.appendChild(templateContent.cloneNode(true));
+	element = document.getElementById(loggedInUser ? 'member-func' : 'non-member-func');
 
-			this.shadowRoot.querySelector('#logoutButton').addEventListener('click', this.logout, false);
-		}
+	if (element) {
+		element.setAttribute('style', 'display: block;');
+	}
 
-		logout() {
-			Parse.User.logOut().catch(() => {}).then(() => {
+	element = document.getElementById('logoutButton');
+
+	if (element) {
+		element.addEventListener('click', () => {
+			Parse.User.logOut().catch(() => { }).then(() => {
 				window.location.href = '/index.html';
 				return false;
 			});
-		}
-
-		connectedCallback() {
-			// prevent flicker
-			window.addEventListener(
-				'load',
-				() => {
-					if (Parse.User.current()) {
-						this.shadowRoot.querySelector('#member').setAttribute('style', 'display: block;');
-					} else {
-						this.shadowRoot.querySelector('#non-member').setAttribute('style', 'display: block;');
-					}
-				},
-				false
-			);
-		}
+		}, false);
 	}
-);
+
+	element = document.getElementById('logoutButton2');
+
+	if (element) {
+		element.addEventListener('click', () => {
+			Parse.User.logOut().catch(() => { }).then(() => {
+				window.location.href = '/index.html';
+				return false;
+			});
+		}, false);
+	}
+
+}, false);
