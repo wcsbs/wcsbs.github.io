b80435279ef17e5f189e3e6eed5b9b02ca7eed51
Donghao Huang <donghao.huang@gmail.com>
2020-11-09 16:53:25 +0800

    refactoring code

--- docs/version.php
+++ docs/version.php
@@ -1,11 +1,13 @@
 <?php
 $API_APP_NAME = "Sandbox";
 $SOURCE_VERSION = "unknown";
-$LOCAL_VERSION = shell_exec("git rev-parse HEAD");
+
+if (strlen($SOURCE_VERSION) == 7) {
+    $SOURCE_VERSION = shell_exec("git rev-parse HEAD");
+}
 
 echo "<h2>WCSBS " . $API_APP_NAME . "</h2>";
-echo "Source Version: " . $SOURCE_VERSION . "<br>";
-echo "Local Version: " . $LOCAL_VERSION . "<br>";
+echo "Version: " . $SOURCE_VERSION . "<br>";
 ?>
 
 <?php phpinfo();
