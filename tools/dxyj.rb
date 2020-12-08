5f8ca98a2a1bc7518e12ffb3da76ddd29c7fd88c
Donghao Huang <donghao.huang@gmail.com>
2020-12-08 22:53:11 +0800

    building DaXueYanJiang

---
+++ tools/dxyj.rb
@@ -0,0 +1,116 @@
+#!/usr/bin/env ruby
+
+require 'nokogiri'
+require 'csv'
+
+def process_one_file(template_file, input_file, title, textbook_link, lineage_link)
+    template = File.read(template_file)
+    template = template.gsub('class_title', title)
+    template = template.gsub('textbook_link', textbook_link)
+    template = template.gsub('lineage_link', lineage_link)
+
+    new_node = Nokogiri::HTML(template)
+  
+    doc = File.open(input_file) { |f| Nokogiri::HTML(f) }
+    node = doc.at_css('article')
+    node.first_element_child.replace(new_node.at_css('div'))
+
+    result = doc.to_s
+        
+    File.open(input_file, 'w') { |file| file.write(result) } 
+    puts "Done for #{doc.title} - #{input_file}"
+end
+
+def process_node(node, template_file1, class_title, class_html, textbook_link, lineage_link)
+    template = File.read(template_file1)
+    template = template.gsub('class_title', class_title)
+    template = template.gsub('class_html', class_html)
+    template = template.gsub('textbook_link', textbook_link)
+    template = template.gsub('lineage_link', lineage_link)
+
+    new_node = Nokogiri::HTML(template)
+    node2 = node.replace(new_node.at_css('div'))
+
+    #puts "Node: #{node2.to_s}"
+end
+
+def pdf2html(input_dir, output_dir, index, name)
+    puts `pdf2htmlEX --zoom 1.5 --embed cfijo --split-pages 1 --dest-dir #{output_dir}/#{index}/ --page-filename #{index}-%d.page #{input_dir}/#{name}.pdf`
+    puts `mv #{output_dir}/#{index}/#{name}.html #{output_dir}/#{index}/index.html`
+    puts `ls -lt #{output_dir}/#{index}/*.html`
+end
+
+def process_index(template_file1, template_file2, input_dir, output_dir)
+    table = CSV.parse(File.read("./csv/self_study_sessions.csv"), headers: true)
+    table.each do |r|
+        index = "%02d" % r[0]
+        name = r[1]
+        onedrive1 = r[2]
+        onedrive2 = r[3]
+        # puts onedrive2
+        # pdf2html(input_dir, output_dir, index, name)
+    end
+    return
+
+    index_file = output_dir + "index.html"
+    puts index_file
+    doc = File.open(index_file) { |f| Nokogiri::HTML(f) }
+    puts doc.title
+
+    i = 0
+    youtube_csv = File.open("./csv/#{class_name}_youtube.csv", 'r')
+
+    doc.css('h1').each do |h1|
+        href = h1.parent['href']
+        puts "Processing #{h1.text} #{href} ..."
+        csv.puts "#{h1.text},../dymqx/#{class_name}/#{href}"
+
+        faben = class_name == "qxgs" ? (i == 0 ? "../doc/前行的重要性.doc" : "../doc/《大圆满前行》讲解第#{i}课.doc") : ""
+        i += 1
+
+        chuancheng = youtube_csv.readline.strip!
+        index = chuancheng.index("https")
+        if index != 0
+            chuancheng = chuancheng[index .. chuancheng.length - index]
+        end
+
+        process_node(h1.parent.parent.parent, template_file1, h1.text, href, faben, chuancheng)
+        
+        input_file  = input_dir + href
+        output_file  = output_dir + href
+        process_one_file(template_file2, input_file, h1.text, faben, chuancheng)
+    end
+
+    result = doc.to_s    
+    File.open(index_file, 'w') { |file| file.write(result) }
+  
+  nil
+end
+
+def main
+    template_file1 = ARGV[0]
+    template_file2 = ARGV[1]
+    input_dir = ARGV[2]
+  
+    output_dir = input_dir
+    if ARGV.length > 3
+        output_dir = ARGV[3]
+    end
+
+    process_index(template_file1, template_file2, input_dir, output_dir)
+end
+
+
+if __FILE__ == $0
+  usage = <<-EOU
+
+usage: ruby #{File.basename($0)} template_file1 template_file2 input_dir (optional)output_dir
+
+  EOU
+
+  abort usage if ARGV.length < 3
+
+  main
+
+end
+#./dym_index.rb templates/qxsx_session.html templates/checkbox.html ../docs/dymqx/qxsx/
